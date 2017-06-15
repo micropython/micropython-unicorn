b9956df6696365d2fafd2c130460e1047b02b18a
Rami Ali <flowergrass@users.noreply.github.com>
2017-06-15 18:06:43 +1000

    www-emu/pyboard_demos.py: Add pyb module demos.

--- www-emu/pyboard_demos.py
+++ www-emu/pyboard_demos.py
@@ -13,7 +13,21 @@ def asm_add(r0, r1):
     add(r0, r0, r1)
 print(asm_add(1, 2))
 #####
-# Activate LEDs
-# each bit represents an LED
-import machine
-machine.mem32[0x40000200] = 0b1111
+# Switch
+# push the USR button on the pyboard to turn the switch on!
+# try using the reset button on the pyboard to quit this script!
+# switch callback not yet supported.
+import pyb
+sw = pyb.Switch()
+b = sw()
+while True:
+    s = sw()
+    if b != s:
+        print(s)
+    b = s
+#####
+# LEDs
+# there are four LEDS numbered 1 to 4
+import pyb
+for i in range(1000):
+    pyb.LED((i%4) + 1).toggle()
