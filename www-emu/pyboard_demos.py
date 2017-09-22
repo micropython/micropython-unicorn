38837060dddc510fb75a0d6f4b5f56a4f193b459
Rami Ali <flowergrass@users.noreply.github.com>
2017-09-22 16:48:52 +1000

    www-emu/pyboard_demos.py: Add I2C LCD demo.

--- www-emu/pyboard_demos.py
+++ www-emu/pyboard_demos.py
@@ -106,6 +106,34 @@ servo = pyb.Servo(1)
 
 servo.angle(90, 5000)
 #####
+# I2C LCD
+# A fully simulated I2C bus and LCD Display
+# The framebuf class simplifies graphics in MicroPython
+# Make sure you have the I2C LCD checkbox marked!
+
+import machine
+import framebuf
+
+scl = machine.Pin('X9')
+sda = machine.Pin('X10')
+i2c = machine.I2C(scl=scl, sda=sda)
+
+fbuf = framebuf.FrameBuffer(bytearray(64 * 32 // 8), 64, 32, framebuf.MONO_HLSB)
+
+logo = framebuf.FrameBuffer(bytearray(17 * 17 // 8), 17, 17, framebuf.MONO_HLSB)
+
+logo.fill(0)
+logo.fill_rect(1, 1, 15, 15, 1)
+logo.vline(4, 4, 12, 0)
+logo.vline(8, 1, 12, 0)
+logo.vline(12, 4, 12, 0)
+logo.vline(14, 13, 2, 0)
+
+fbuf.fill(0)
+fbuf.blit(logo, 23, 7)
+
+i2c.writeto(8, fbuf)
+#####
 # MANDELBROT SET
 # A python Mandelbrot set courtesy of 
 # http://warp.povusers.org/MandScripts/python.html
